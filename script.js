05648e1c452d53d3a66425c9597fd0ecf752c826
BanzEM <bandisilemazomba123@gmail.com>
2026-03-19 11:31:39 +0200

    fixed loop for showing first 10 data

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ fetch("https://my-json-server.typicode.com/BanzEm/api/transactions")
 
     let output = document.querySelector("#json-output")
     let data = "";
-    let i = 0
+    let i = 0;
 
+    
     for(let product of transactions){
-        while(i < 10){
-        data += `
-        
-        <tr>
-            <th>${product.id}</th>
-            <td>${product.transaction}</td>
-            <td>${product.amount}</td>
-            <td>${product.date}</td>
-            <td>${product.status}</td>
-          </tr>
-        
-        `;
-        i++
-    }
+
+        if(i < 10){
+            data += `   
+            <tr>
+                <th>${product.id}</th>
+                <td>${product.transaction}</td>
+                <td>${product.amount}</td>
+                <td>${product.date}</td>
+                <td>${product.status}</td>
+            </tr>
+            
+            `;
+            i++
+        } else{
+            
+            break;
+        }
+    
 }
 
 output.innerHTML = data;
