6682172c4a2aecbecf8e1c54dac978a2cc8eeae0
BanzEM <bandisilemazomba123@gmail.com>
2026-03-19 13:50:02 +0200

    put color of status

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-fetch("https://my-json-server.typicode.com/BanzEm/api/transactions")
+fetch("https://my-json-server.typicode.com/BanzEm/js-api/transactions")
 .then(response => response.json())
 .then(function(transactions){
 
     let output = document.querySelector("#json-output")
     let data = "";
+
     let i = 0;
+    let transation_count = 0;
 
     
     for(let product of transactions){
 
-        if(i < 10){
+        if (i < 10){
             data += `   
             <tr>
                 <th>${product.id}</th>
                 <td>${product.transaction}</td>
                 <td>${product.amount}</td>
                 <td>${product.date}</td>
-                <td>${product.status}</td>
+                <td >
+                <span class="${product.status == "Success" ? 'text-green-600' : 'text-yellow-500'}"> ${product.status} </span>
+                </td>
             </tr>
-            
             `;
-            i++
-        } else{
-            
-            break;
-        }
-    
-}
 
+            transation_count++
+            i++
+
+        } else{
+            const intervalCounter = setInterval(table_rows,3000)
+
+            function table_rows(){
+                data += data
+             
+               }
+            transation_count++
+
+        }
+
+    }        
 output.innerHTML = data;
 
 })
+.catch(error => {
+    console.error('Error:', error);
+  });
+
+
+  
