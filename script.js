dec27078930a00ef3b6c6d3914f35062980bdebf
BanzEM <bandisilemazomba123@gmail.com>
2026-03-19 17:34:30 +0200

    rows and data adding as they should

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ fetch("https://my-json-server.typicode.com/BanzEm/js-api/transactions")
 .then(function(transactions){
 
     let output = document.querySelector("#json-output")
+    let counter = document.querySelector("#transaction_count")
+
     let data = "";
 
     let i = 0;
@@ -14,7 +16,7 @@ fetch("https://my-json-server.typicode.com/BanzEm/js-api/transactions")
         if (i < 10){
             data += `   
             <tr>
-                <th>${product.id}</th>
+                <td>${product.id}</td>
                 <td>${product.transaction}</td>
                 <td>${product.amount}</td>
                 <td>${product.date}</td>
@@ -24,22 +26,49 @@ fetch("https://my-json-server.typicode.com/BanzEm/js-api/transactions")
             </tr>
             `;
 
-            transation_count++
-            i++
+            transation_count++ 
 
+        } else if ( i >= 9){
+
+       
+
+        }          
+        
+        i++
+
+    } 
+
+    let index = 10
+
+    function addRow(){
+
+        if(index < transactions.length){
+        var table = document.getElementById("transaction-table");
+        var row = table.insertRow(1);
+        var column1 = row.insertCell(0);
+        var column2 = row.insertCell(1);
+        var column3 = row.insertCell(2);
+        var column4 = row.insertCell(3);
+        var column5 = row.insertCell(4);
+        
+        column1.innerHTML = transactions[index].id;
+        column2.innerHTML = transactions[index].transaction;
+        column3.innerHTML = transactions[index].amount;
+        column4.innerHTML = transactions[index].date;
+        column5.innerHTML = transactions[index].status;    
+
+        index++
         } else{
-            const intervalCounter = setInterval(table_rows,3000)
-
-            function table_rows(){
-                data += data
-             
-               }
-            transation_count++
-
+            clearInterval(interval)
         }
+        }
+    
+        setInterval(addRow,10000)
+        transation_count++ 
 
-    }        
+
 output.innerHTML = data;
+counter.innerHTML = transation_count;
 
 })
 .catch(error => {
@@ -47,4 +76,3 @@ output.innerHTML = data;
   });
 
 
-  
