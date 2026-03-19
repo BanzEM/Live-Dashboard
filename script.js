b9cca808a7257efb05830f4634f1356b68e24bda
BanzEM <bandisilemazomba123@gmail.com>
2026-03-19 10:56:32 +0200

    adding javascript loop to show first 10 transactions

---
+++ script.js
@@ -0,0 +1,28 @@
+fetch("https://my-json-server.typicode.com/BanzEm/api/transactions")
+.then(response => response.json())
+.then(function(transactions){
+
+    let output = document.querySelector("#json-output")
+    let data = "";
+    let i = 0
+
+    for(let product of transactions){
+        while(i < 10){
+        data += `
+        
+        <tr>
+            <th>${product.id}</th>
+            <td>${product.transaction}</td>
+            <td>${product.amount}</td>
+            <td>${product.date}</td>
+            <td>${product.status}</td>
+          </tr>
+        
+        `;
+        i++
+    }
+}
+
+output.innerHTML = data;
+
+})
